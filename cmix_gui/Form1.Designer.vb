00c0495ae845b789a79c9f2acd81dd518bc48988
moisesmcardona <moises@moises-studios.com>
2018-05-22 12:16:05 -0400

    GUI code

--- cmix_gui/Form1.Designer.vb
+++ cmix_gui/Form1.Designer.vb
@@ -22,10 +22,252 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.InputFileMessage = New System.Windows.Forms.Label()
+        Me.InputFileTxt = New System.Windows.Forms.TextBox()
+        Me.OutputFileMessage = New System.Windows.Forms.Label()
+        Me.OutputFileTxt = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ExtractRButton = New System.Windows.Forms.RadioButton()
+        Me.CompressRButton = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmixVersionDropdown = New System.Windows.Forms.ComboBox()
+        Me.BrowseButton1 = New System.Windows.Forms.Button()
+        Me.BrowseButton2 = New System.Windows.Forms.Button()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BrowseFolder = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'InputFileMessage
+        '
+        Me.InputFileMessage.AutoSize = True
+        Me.InputFileMessage.Location = New System.Drawing.Point(9, 70)
+        Me.InputFileMessage.Name = "InputFileMessage"
+        Me.InputFileMessage.Size = New System.Drawing.Size(223, 13)
+        Me.InputFileMessage.TabIndex = 0
+        Me.InputFileMessage.Text = "Step 1: Browse for a file or folder to compress:"
+        '
+        'InputFileTxt
+        '
+        Me.InputFileTxt.Location = New System.Drawing.Point(12, 86)
+        Me.InputFileTxt.Name = "InputFileTxt"
+        Me.InputFileTxt.Size = New System.Drawing.Size(237, 20)
+        Me.InputFileTxt.TabIndex = 1
+        '
+        'OutputFileMessage
+        '
+        Me.OutputFileMessage.AutoSize = True
+        Me.OutputFileMessage.Location = New System.Drawing.Point(9, 109)
+        Me.OutputFileMessage.Name = "OutputFileMessage"
+        Me.OutputFileMessage.Size = New System.Drawing.Size(240, 13)
+        Me.OutputFileMessage.TabIndex = 2
+        Me.OutputFileMessage.Text = "Step 2: Browse for a location to save the archive:"
+        '
+        'OutputFileTxt
+        '
+        Me.OutputFileTxt.Location = New System.Drawing.Point(12, 125)
+        Me.OutputFileTxt.Name = "OutputFileTxt"
+        Me.OutputFileTxt.Size = New System.Drawing.Size(237, 20)
+        Me.OutputFileTxt.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ExtractRButton)
+        Me.GroupBox1.Controls.Add(Me.CompressRButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(150, 46)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "I want to:"
+        '
+        'ExtractRButton
+        '
+        Me.ExtractRButton.AutoSize = True
+        Me.ExtractRButton.Location = New System.Drawing.Point(83, 19)
+        Me.ExtractRButton.Name = "ExtractRButton"
+        Me.ExtractRButton.Size = New System.Drawing.Size(58, 17)
+        Me.ExtractRButton.TabIndex = 1
+        Me.ExtractRButton.TabStop = True
+        Me.ExtractRButton.Text = "Extract"
+        Me.ExtractRButton.UseVisualStyleBackColor = True
+        '
+        'CompressRButton
+        '
+        Me.CompressRButton.AutoSize = True
+        Me.CompressRButton.Location = New System.Drawing.Point(6, 19)
+        Me.CompressRButton.Name = "CompressRButton"
+        Me.CompressRButton.Size = New System.Drawing.Size(71, 17)
+        Me.CompressRButton.TabIndex = 0
+        Me.CompressRButton.TabStop = True
+        Me.CompressRButton.Text = "Compress"
+        Me.CompressRButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "cmix version to use:"
+        '
+        'cmixVersionDropdown
+        '
+        Me.cmixVersionDropdown.FormattingEnabled = True
+        Me.cmixVersionDropdown.Items.AddRange(New Object() {"cmix_v15b"})
+        Me.cmixVersionDropdown.Location = New System.Drawing.Point(12, 164)
+        Me.cmixVersionDropdown.Name = "cmixVersionDropdown"
+        Me.cmixVersionDropdown.Size = New System.Drawing.Size(121, 21)
+        Me.cmixVersionDropdown.TabIndex = 6
+        '
+        'BrowseButton1
+        '
+        Me.BrowseButton1.Location = New System.Drawing.Point(255, 84)
+        Me.BrowseButton1.Name = "BrowseButton1"
+        Me.BrowseButton1.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseButton1.TabIndex = 7
+        Me.BrowseButton1.Text = "Browse File"
+        Me.BrowseButton1.UseVisualStyleBackColor = True
+        '
+        'BrowseButton2
+        '
+        Me.BrowseButton2.Location = New System.Drawing.Point(255, 123)
+        Me.BrowseButton2.Name = "BrowseButton2"
+        Me.BrowseButton2.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseButton2.TabIndex = 8
+        Me.BrowseButton2.Text = "Browse"
+        Me.BrowseButton2.UseVisualStyleBackColor = True
+        '
+        'StartButton
+        '
+        Me.StartButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StartButton.Location = New System.Drawing.Point(139, 162)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(191, 23)
+        Me.StartButton.TabIndex = 9
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 207)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "GUI software by: Moisés Cardona"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 220)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "cmix by: Byron Knoll"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 233)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "cmix GitHub Repo:"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(114, 233)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(174, 13)
+        Me.LinkLabel1.TabIndex = 13
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://github.com/byronknoll/cmix"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(375, 233)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "GUI v1.0"
+        '
+        'BrowseFolder
+        '
+        Me.BrowseFolder.Location = New System.Drawing.Point(336, 84)
+        Me.BrowseFolder.Name = "BrowseFolder"
+        Me.BrowseFolder.Size = New System.Drawing.Size(89, 23)
+        Me.BrowseFolder.TabIndex = 15
+        Me.BrowseFolder.Text = "Browse Folder"
+        Me.BrowseFolder.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(441, 257)
+        Me.Controls.Add(Me.BrowseFolder)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.BrowseButton2)
+        Me.Controls.Add(Me.BrowseButton1)
+        Me.Controls.Add(Me.cmixVersionDropdown)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.OutputFileTxt)
+        Me.Controls.Add(Me.OutputFileMessage)
+        Me.Controls.Add(Me.InputFileTxt)
+        Me.Controls.Add(Me.InputFileMessage)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "cmix Graphical User Interface"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents InputFileMessage As Label
+    Friend WithEvents InputFileTxt As TextBox
+    Friend WithEvents OutputFileMessage As Label
+    Friend WithEvents OutputFileTxt As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ExtractRButton As RadioButton
+    Friend WithEvents CompressRButton As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmixVersionDropdown As ComboBox
+    Friend WithEvents BrowseButton1 As Button
+    Friend WithEvents BrowseButton2 As Button
+    Friend WithEvents StartButton As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents BrowseFolder As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
 End Class
--- cmix_gui/Form1.vb
+++ cmix_gui/Form1.vb
@@ -1,3 +1,147 @@
 ﻿Public Class Form1
+    Private OutputFileName As String = String.Empty
+    Private cmix_version As String = String.Empty
+    Private Sub CompressRButton_CheckedChanged(sender As Object, e As EventArgs) Handles CompressRButton.CheckedChanged
+        InputFileMessage.Text = My.Resources.CompressInputMessage
+        OutputFileMessage.Text = My.Resources.CompressOutputMessage
+        BrowseFolder.Enabled = True
+        My.Settings.Compress = CompressRButton.Checked
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ExtractRButton_CheckedChanged(sender As Object, e As EventArgs) Handles ExtractRButton.CheckedChanged
+        InputFileMessage.Text = My.Resources.ExtractInputMessage
+        OutputFileMessage.Text = My.Resources.ExtractOutputMessage
+        BrowseFolder.Enabled = False
+        My.Settings.Extract = ExtractRButton.Checked
+        My.Settings.Save()
+    End Sub
+
+    Private Function CheckIfFileOrFolder(PathToCheck As String) As String
+        If My.Computer.FileSystem.FileExists(PathToCheck) Then
+            OutputFileMessage.Text = My.Resources.CompressOutputMessage
+            OutputFileTxt.Enabled = True
+            BrowseButton2.Enabled = True
+            Return "File"
+        ElseIf My.Computer.FileSystem.DirectoryExists(PathToCheck) Then
+            OutputFileMessage.Text = My.Resources.CompressFolderSelectedMessage
+            OutputFileTxt.Enabled = False
+            BrowseButton2.Enabled = False
+            Return "Folder"
+        End If
+        Return "N/A"
+    End Function
+    Private Sub BrowseButton1_Click(sender As Object, e As EventArgs) Handles BrowseButton1.Click
+        OpenFileDialog1.Title = InputFileTxt.Text
+        OpenFileDialog1.Filter = "All files (*.*)|*.*"
+        If InputFileTxt.Text IsNot String.Empty Then
+            If My.Computer.FileSystem.FileExists(InputFileTxt.Text) Then OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(InputFileTxt.Text) Else OpenFileDialog1.FileName = String.Empty
+        End If
+        Dim response As DialogResult = OpenFileDialog1.ShowDialog
+        If response = DialogResult.OK Then
+            InputFileTxt.Text = OpenFileDialog1.FileName
+            CheckIfFileOrFolder(InputFileTxt.Text)
+        End If
+    End Sub
+
+    Private Sub BrowseFolder_Click(sender As Object, e As EventArgs) Handles BrowseFolder.Click
+        Dim response As DialogResult = FolderBrowserDialog1.ShowDialog()
+        If response = DialogResult.OK Then
+            InputFileTxt.Text = FolderBrowserDialog1.SelectedPath
+            CheckIfFileOrFolder(InputFileTxt.Text)
+        End If
+    End Sub
+
+    Private Sub BrowseButton2_Click(sender As Object, e As EventArgs) Handles BrowseButton2.Click
+        SaveFileDialog1.Title = InputFileTxt.Text
+        If CompressRButton.Checked Then SaveFileDialog1.Filter = "cmix file|*.cmix" Else SaveFileDialog1.Filter = "All files (*.*)|*.*"
+        If OutputFileTxt.Text IsNot String.Empty Then
+            If My.Computer.FileSystem.FileExists(OutputFileTxt.Text) Then SaveFileDialog1.FileName = My.Computer.FileSystem.GetName(OutputFileTxt.Text) Else SaveFileDialog1.FileName = String.Empty
+        End If
+        Dim response As DialogResult = SaveFileDialog1.ShowDialog
+        If response = DialogResult.OK Then
+            OutputFileName = SaveFileDialog1.FileName
+            If CompressRButton.Checked Then SetOutputFilename()
+        End If
+    End Sub
+
+    Private Sub SetOutputFilename()
+        If cmixVersionDropdown.SelectedItem = "cmix_v15b" Then
+            OutputFileTxt.Text = OutputFileName + "15b"
+            cmix_version = "15b"
+        End If
+    End Sub
+
+    Private Sub cmixVersionDropdown_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmixVersionDropdown.SelectedIndexChanged
+        My.Settings.Version = cmixVersionDropdown.SelectedItem
+        My.Settings.Save()
+        If OutputFileName IsNot String.Empty Then
+            SetOutputFilename()
+        End If
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        InputFileTxt.Text = e.Data.GetData(DataFormats.FileDrop)(0)
+        CheckIfFileOrFolder(InputFileTxt.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CompressRButton.Checked = My.Settings.Compress
+        ExtractRButton.Checked = My.Settings.Extract
+        cmixVersionDropdown.SelectedItem = My.Settings.Version
+    End Sub
+
+    Private Sub Run_cmix(Input As String, Output As String, action As String)
+        Dim cmixProcessInfo As New ProcessStartInfo
+        Dim cmixProcess As Process
+        cmixProcessInfo.FileName = My.Settings.Version + ".exe"
+        cmixProcessInfo.Arguments = action + " """ & Input & """ """ & Output & """"
+        cmixProcessInfo.CreateNoWindow = False
+        cmixProcess = Process.Start(cmixProcessInfo)
+        cmixProcess.WaitForExit()
+    End Sub
+
+    Public Sub ProcessFiles(Folder As String, Action As String)
+        For Each File In IO.Directory.GetFiles(Folder)
+            Run_cmix(File, File + ".cmix" + cmix_version, Action)
+        Next
+    End Sub
+
+    Public Sub ProcessSubfolders(Folder As String, Action As String)
+        For Each Subfolder In IO.Directory.GetDirectories(Folder)
+            ProcessFolder(Subfolder, Action)
+        Next
+    End Sub
+
+    Public Sub ProcessFolder(Folder As String, Action As String)
+        ProcessFiles(Folder, Action)
+        ProcessSubfolders(Folder, Action)
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        If InputFileTxt.Text IsNot String.Empty Then
+            Dim ProcessAction As String = String.Empty
+            If CompressRButton.Checked Then ProcessAction = "-c" Else ProcessAction = "-d"
+            Dim CheckInput As String = CheckIfFileOrFolder(InputFileTxt.Text)
+            If CheckInput = "File" Then
+                If OutputFileTxt.Text IsNot String.Empty Then
+                    Run_cmix(InputFileTxt.Text, OutputFileTxt.Text, ProcessAction)
+                End If
+            ElseIf CheckInput = "Folder" Then
+                ProcessFolder(InputFileTxt.Text, ProcessAction)
+            End If
+            MessageBox.Show("Finished!")
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://github.com/byronknoll/cmix")
+    End Sub
 
 End Class
